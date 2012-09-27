0263beda6a7352d3e0294788b925c3da93f50c53
stdweird <stijn.deweirdt@ugent.be>
2012-09-27 19:39:05 +0200

    working scoop with simple_shell

---
+++ lib/vsc/mympirun/scoop/__main__.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+#
+#    This file is part of Scalable COncurrent Operations in Python (SCOOP).
+#
+#    SCOOP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    SCOOP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with SCOOP. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Some modifications for mympirun
+    Stijn De Weirdt 2012
+    It seems impossible to start it from the myscoop itself
+"""
+from __future__ import print_function
+from vsc.fancylogger import getLogger, setLogLevelDebug, logToFile, disableDefaultHandlers
+
+
+
+import sys
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    import backportRunpy as runpy
+else:
+    import runpy
+import os
+import functools
+import argparse
+import scoop
+
+
+
+def make_parser():
+    """scoop.bootstrap.__main__ parser"""
+    parser = argparse.ArgumentParser(description='Starts the executable.',
+                                     prog="{0} -m scoop.bootstrap".format(sys.executable))
+
+    parser.add_argument('--origin', help="To specify that the worker is the origin",
+                        action='store_true')
+    parser.add_argument('--workerName', help="The name of the worker",
+                        default="worker0")
+    parser.add_argument('--brokerName', help="The name of the broker",
+                        default="broker")
+    parser.add_argument('--brokerAddress',
+                        help="The tcp address of the broker written tcp://address:port",
+                        default="")
+    parser.add_argument('--metaAddress',
+                        help="The tcp address of the info written tcp://address:port",
+                        default="")
+    parser.add_argument('--size',
+                        help="The size of the worker pool",
+                        type=int,
+                        default=1)
+    parser.add_argument('--debug',
+                        help="Activate the debug",
+                        action='store_true')
+    parser.add_argument('--profile',
+                         help="Activate the profiler",
+                         action='store_true')
+    parser.add_argument('executable',
+                        nargs=1,
+                        help='The executable to start with scoop')
+    parser.add_argument('args',
+                        nargs=argparse.REMAINDER,
+                        help='The arguments to pass to the executable',
+                        default=[])
+    ## custom options
+    parser.add_argument('--startfrom', help="Change to this directory on start", action='store', default=None)
+    parser.add_argument('--nice', help="Set this nice level", action='store', default=0, type=int)
+    parser.add_argument('--affinity', help="Use this cpu affinity", action='store', default=None, type=int)
+
+    return parser
+
+def main(args=None):
+    """The scoop.bootstrap.__main__ main()"""
+    if args is None:
+        parser = make_parser()
+        args = parser.parse_args()
+
+    # Setup the scoop constants
+    scoop.IS_ORIGIN = args.origin
+    scoop.WORKER_NAME = args.workerName.encode()
+    scoop.BROKER_NAME = args.brokerName.encode()
+    scoop.BROKER_ADDRESS = args.brokerAddress.encode()
+    scoop.META_ADDRESS = args.metaAddress.encode()
+    scoop.SIZE = args.size
+    scoop.DEBUG = args.debug
+    scoop.IS_ORIGIN = args.origin
+    scoop.worker = (scoop.WORKER_NAME, scoop.BROKER_NAME)
+    scoop.VALID = True
+
+    _logger.debug("main: workerName %s executable %s args %s" % (args.workerName, args.executable, args.args))
+    _logger.debug("main: startfrom %s nice %s affinity %s" % (args.startfrom, args.nice, args.affinity))
+
+    profile = True if args.profile else False
+
+    # get the module path in the Python path
+    md_path = os.path.join(os.getcwd(), os.path.dirname(args.executable[0]))
+    if not md_path in sys.path:
+        sys.path.append(md_path)
+
+
+    # temp values to keep the args
+    executable = args.executable[0]
+
+    # Add the user arguments to argv
+    sys.argv = sys.argv[:1]
+    sys.argv += args.args
+
+    # import the user module into the global dictionary
+    # equivalent to from {user_module} import *
+    user_module = __import__(os.path.basename(executable)[:-3])
+    try:
+        attrlist = user_module.__all__
+    except AttributeError:
+        attrlist = dir(user_module)
+    for attr in attrlist:
+        globals()[attr] = getattr(user_module, attr)
+
+    if not profile:
+        # Start the user program
+        from scoop import futures
+        futures._startup(functools.partial(runpy.run_path,
+                                       executable,
+                                       init_globals=globals(),
+                                       run_name="__main__"))
+    else:
+        from scoop import futures
+        import cProfile
+        cProfile.run("""futures._startup(functools.partial(runpy.run_path,
+                                       executable,
+                                       init_globals=globals(),
+                                       run_name="__main__"))""",
+                                       scoop.WORKER_NAME)
+
+    _logger.debug("main: bootstrap main ended")
+
+
+if __name__ == "__main__":
+    NAME = "MYSCOOPBOOTSTRAP"
+    _logger = getLogger(NAME)
+    parser = make_parser()
+    args = parser.parse_args()
+
+
+    if args.debug:
+        setLogLevelDebug()
+    log_fn = "/tmp/scoop_%s.log" % args.workerName
+    ## TODO: touch and check ownership/permission
+    logToFile(log_fn)
+
+    disableDefaultHandlers()
+
+
+    try:
+        main(args)
+    except:
+        _logger.exception("main bootstrap failed attempt")
---
+++ lib/vsc/mympirun/scoop/picalc.py
@@ -0,0 +1,49 @@
+##
+# Copyright 2012 Stijn De Weirdt
+#
+# This file is part of VSC-tools,
+# originally created by the HPC team of the University of Ghent (http://ugent.be/hpc).
+#
+#
+# http://github.com/hpcugent/VSC-tools
+#
+# VSC-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VSC-tools. If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+SCOOP piCalc exmaple module
+"""
+
+NAME = 'SCOOP_piCalc'
+
+
+from math import hypot
+from random import random
+from scoop import futures
+from time import time
+
+# A range is used in this function for python3. If you are using python2, a
+# xrange might be more efficient.
+def test(tries):
+    return sum(hypot(random(), random()) < 1 for i in range(tries))
+
+# Calculates pi with a Monte-Carlo method. This function calls the function
+# test "n" times with an argument of "t". Scoop dispatches these
+# functions interactively accross the available ressources.
+def calcPi(workers, tries):
+    expr = futures.map(test, [tries] * workers)
+    piValue = 4. * sum(expr) / float(workers * tries)
+    return piValue
+
+
+if __name__ == '__main__':
+    print calcPi(3000, 5000)
